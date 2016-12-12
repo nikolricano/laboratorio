5ea5b91747c3164db1cc3527f83f81281c4827a0
Nikol <nricano87@gmail.com>
2016-12-12 06:10:45 -0600

    falta arreglar la barra de menu de resultados

--- app/controllers/laboratorio_controller.rb
+++ app/controllers/laboratorio_controller.rb
@@ -1,15 +1,14 @@
 class LaboratorioController < ApplicationController
+
   def bienvenidos
   end
 
-  def resultados
-    render layout: "barranavegacion"
-  end
 
   def resultados
     authenticate_user!
     current_user
   end
+
 
   def send_mail
     BasicMailer.basic_mail.deliver_now
@@ -20,9 +19,9 @@ class LaboratorioController < ApplicationController
       current_user
       current_user.galleries << Gallery.create(file: params[:user][:file])
       if current_user.save
-        redirect_to root_path, alert: 'Imagen bien'
+        redirect_to laboratorio_resultados_path, alert: 'Imagen bien'
       else
-        redirect_to root_path, alert: 'Imagen mal'
+        redirect_to laboratorio_resultados_path, alert: 'Imagen mal'
       end
     end
 
